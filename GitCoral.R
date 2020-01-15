8f47bb0c16121cab43a9bea663bc34162c18d903
nsantodomingo <nsantodomingo@gmail.com>
2020-01-15 12:10:44 +0000

    commit this project

--- GitCoral.R
+++ GitCoral.R
@@ -1,1 +1,14 @@
 ## Learning Git
+
+# today is learning day
+
+compensation <- read.csv ("compensation.csv")
+
+# I will
+#because it is a bit boring
+ #this is because I like to write 
+
+kjasg´ksajgáskgjas´lk
+lga´ñglja´
+
+
